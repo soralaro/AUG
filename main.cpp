347081d31190ab3e24fc74bd9224c936eac8b38c
zhenxiongchen <7755120@gmail.com>
2019-09-12 19:14:00 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "hiaiengine/engine.h"
+#include "hiaiengine/data_type.h"
+#include "dvpp/Vpc.h"
+#include "hiaiengine/multitype_queue.h"
+#include "dvpp/idvppapi.h"
+#include <iostream>
+#include <string>
+#include <dirent.h>
+#include <memory>
+#include <unistd.h>
+#include <vector>
+#include <stdint.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
